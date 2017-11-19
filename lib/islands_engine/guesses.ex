981e21e88468a03252425958de9fc308cbae3523
nvick <nate.vick@icloud.com>
2017-11-19 08:46:14 -0800

    Define all initial entities and functions

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Guesses do
-  alias __MODULE__
+  alias IslandsEngine.{Coordinate, Guesses}
   @moduledoc """
   This module will hold all guesses ans logic around it.
   """
@@ -8,5 +8,13 @@ defmodule IslandsEngine.Guesses do
   defstruct [:hits, :misses]
 
   def new(), do: %Guesses{hits: MapSet.new(), misses: MapSet.new()}
+
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coordinate) do
+    update_in(guesses.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
+    update_in(guesses.misses, &MapSet.put(&1, coordinate))
+  end
 
 end
